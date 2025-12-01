f87fda1e3e2ca359ccdc4b2751bf929186d40dbd
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2025-12-01 18:02:10 +0100

    FIX: delete log

--- src/pipeline.py
+++ src/pipeline.py
@@ -9,14 +9,13 @@ logger = logging.getLogger("heatmap_creator")
 def run_pipeline(args):
     config = set_configuration(args)
     data = data_preprocessing_pipeline(config)
-    logger.debug("Current results is %s syntagmae", len(data))
+    logger.debug("Current results is %s texts", len(data))
     if config.data_type == "text_variation":
         for idx, val in enumerate(data):
             create_heatmap(val, config.heatmap_params, idx)
     elif config.data_type == "pos":
         params = config.heatmap_params
         for idx, val in enumerate(data):
-            print(idx)
             create_heatmap_from_matrix(
                     val, 
                     params.output_dir, params.is_continuous,
